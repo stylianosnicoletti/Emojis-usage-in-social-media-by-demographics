fe11c074417872d175a56e2e0bd16084d947b07e
Stylianos Nicoletti <s1516821@tarpon.inf.ed.ac.uk>
2018-10-24 22:30:29 +0100

    sample to pandas

--- tweets_to_panda.py
+++ tweets_to_panda.py
@@ -1,18 +1,19 @@
 import json
 import pandas as pd
 
+# Path of tweets sample
+tweets_data_path = 'tweets_sample.txt'
 
-tweets_data_path = 'twitter_data.txt'
-
-tweets_data = []
+# Open tweets sample
 tweets_file = open(tweets_data_path, "r")
 
+# To store sample data with attributes needed
 data = {'text': [], 'lang': [], 'country': [], 'prof_picture': []}
 
 # An empty list to store emojis
 emojis_list =[]
 
-# Json of emojis to list
+# Function: Json of emojis to list (1640 emojis) 
 def fill_emojis_list(list,emojis_json):
 	with open(emojis_json) as f:
 		data = json.load(f)
@@ -22,28 +23,25 @@ def fill_emojis_list(list,emojis_json):
 		print repr(element['emoji'])
 		print element['emoji']
 		list.append(element['emoji'])
-		print(len(list))				# 1640 emojis 
+		print(len(list))				
 
+# Function: Create a panda dataframe with the attributes needed
+def sample_to_panda_dataframe(data_attributes,file):
+	for line in file:
+		try:
+			tweet = json.loads(line)
+			data_attributes['text'].append(tweet['text'])
+			data_attributes['lang'].append(tweet['lang'])
+			data_attributes['country'].append(tweet['place']['country']if tweet['place'] != None else None)
+			# Trim size normal from picture url so as to display original size profile picture
+			data_attributes['prof_picture'].append("{}{}".format(tweet['user']['profile_image_url_https'][:-11],".jpg"))
+		except:
+			continue
+	tweets = pd.DataFrame(data_attributes)
+	print tweets
 
-#tweets = pd.DataFrame(data)
+sample_to_panda_dataframe(data,tweets_file)
 
-for line in tweets_file:
-	try:
-		tweet = json.loads(line)
-		tweets_data.append(tweet)
-		data['text'].append(tweet['text'])
-		data['lang'].append(tweet['lang'])
-		data['country'].append(tweet['place']['country']if tweet['place'] != None else None)
-		# Trim size normal from picture url so as to display original size profile picture
-		data['prof_picture'].append("{}{}".format(tweet['user']['profile_image_url_https'][:-11],".jpg"))
-	except:
-		continue
-
-#print len(tweets_data)
-tweets = pd.DataFrame(data)
-#pd.set_option('display.max_colwidth',1000)
-#print tweets
-#print str(tweets['prof_picture'])
 
 
 	# Fill the list of 1640 emojis
